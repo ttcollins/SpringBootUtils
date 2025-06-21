7f492143161684da02c99e44e03073e11d063132
Tamale Talemwa Collins <51233620+ttcollins@users.noreply.github.com>
2025-06-21 07:05:30 +0300

    Add fields to ActivityDto

--- todoapp/src/main/java/org/savea/todoapp/controllers/ActivityDto.java
+++ todoapp/src/main/java/org/savea/todoapp/controllers/ActivityDto.java
@@ -1,4 +1,13 @@
 package org.savea.todoapp.controllers;
 
-public record ActivityDto() {
-}
+import org.savea.todoapp.config.DiffUtil.Change;
+
+import java.util.Date;
+import java.util.Map;
+
+public record ActivityDto(
+    long revisionId,
+    Date timestamp,
+    String username,
+    Map<String, Change<?>> diff
+) {}
